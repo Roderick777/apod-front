95b97d207fff394734ee4517a7f5bcf8dd2ba6bb
R González <rodrigoalexissss@gmail.com>
2021-10-17 13:45:32 -0300

    change url services

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,14 +1,14 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 export class Service {
-  public static baseUrl = 'http://localhost:3333/';
-  // public static baseUrl = 'http://147.182.255.212/';
+  // public static baseUrl = 'http://localhost:3333/';
+  public static baseUrl = 'http://147.182.255.212/'
 
   public static async get<T>(url: string) {
     const data: AxiosResponse<T> = await axios.get(
       this.baseUrl + url,
       this.getHeader(url)
-    );
-    return data.data;
+    )
+    return data.data
   }
 
   public static async post<T, T2>(url: string, body: T2) {
@@ -16,8 +16,8 @@ export class Service {
       this.baseUrl + url,
       body,
       this.getHeader(url)
-    );
-    return data.data;
+    )
+    return data.data
   }
 
   public static async put<T, T2>(url: string, body: T2) {
@@ -25,28 +25,28 @@ export class Service {
       this.baseUrl + url,
       body,
       this.getHeader(url)
-    );
-    return data.data;
+    )
+    return data.data
   }
 
   public static async delete<T>(url: string) {
     const data: AxiosResponse<T> = await axios.delete(
       this.baseUrl + url,
       this.getHeader(url)
-    );
-    return data.data;
+    )
+    return data.data
   }
 
   public static getHeader(url: string) {
-    const excludeUrls = ['login'];
+    const excludeUrls = ['login']
     if (!excludeUrls.includes(url)) {
       return {
         headers: {
           Authorization: 'Bearer ' + localStorage.getItem('token'),
         },
-      } as AxiosRequestConfig;
+      } as AxiosRequestConfig
     } else {
-      return {};
+      return {}
     }
   }
 }
